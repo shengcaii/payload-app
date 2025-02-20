fcfeb2ff9bd7afbc83d6e12abbb5a9f7d603c272
Thiha Aung <thihaaung101543@gmail.com>
2025-02-20 13:39:23 +0630

    configure vercel blob

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -203,6 +203,7 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -962,6 +963,7 @@ export interface PostsSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -16,6 +16,7 @@ import { Header } from './Header/config'
 import { plugins } from './plugins'
 import { defaultLexical } from '@/fields/defaultLexical'
 import { getServerSideURL } from './utilities/getURL'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob';
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -67,6 +68,14 @@ export default buildConfig({
   plugins: [
     ...plugins,
     // storage-adapter-placeholder
+    vercelBlobStorage({
+      collections:{
+        'media':{
+          prefix: 'media'
+        }
+      },
+      token:process.env.BLOB_READ_WRITE_TOKEN
+    }),
   ],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
